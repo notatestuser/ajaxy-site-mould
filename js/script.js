5c0377fb05d2759a3e2d9467ae5214c7468cabca
Luke Plaster <notatestuser@gmail.com>
2013-03-03 02:53:23 +0000

    namespaced everything within the main script(.js) to avoid pollution as things are added

--- js/script.js
+++ js/script.js
@@ -3,85 +3,90 @@
    December 2012
 */
 
-var PAGE_FILE_DIR = '';
-var PAGE_FILE_EXT = '.php';
+(function(w){
 
-var lukep = lukep || { pageRequestInProgress: true }; // we'll prevent an ajax request going out for initial load
+	var PAGE_FILE_DIR = '';
+	var PAGE_FILE_EXT = '.php';
 
-var rewireHyperlinks = function(contextEl){
-	// upgrade hyperlink functionality in JS-able browsers: use hashes instead
-	$('a', contextEl).each(function(){
-		var $this = $(this),
-			href = $this.attr('href');
-		if (href.indexOf('#!/') !== 0 && href.indexOf('http') !== 0 && href.indexOf('mailto:') !== 0) {
-			$this.attr('href', '#!/'+href);
-		}
-	});
+	// we'll prevent an ajax request going out for initial load
+	var app = w.app = w.app || { pageRequestInProgress: true };
 
-	// hook up hyperlink functionality to eligible list items containing anchors
-	$('li > a, h1 a', contextEl).each(function(){
-		var $anchor = $(this);
-		var $parentItem = $anchor.parent();
-		if ($parentItem.css('cursor') == 'pointer') {
-			$parentItem.attr('alt', $anchor.attr('href'));
-			$anchor.removeAttr('href');
-			$parentItem.click(function(){
-				var href = $parentItem.attr('alt');
-				if (href != location.href) {
-					location.href = href;
-				}
-			});
-		}
-	});
-};
+	var rewireHyperlinks = function(contextEl){
+		// upgrade hyperlink functionality in JS-able browsers: use hashes instead
+		$('a', contextEl).each(function(){
+			var $this = $(this),
+				href = $this.attr('href');
+			if (href.indexOf('#!/') !== 0 && href.indexOf('http') !== 0 && href.indexOf('mailto:') !== 0) {
+				$this.attr('href', '#!/'+href);
+			}
+		});
 
-$(document).ready(function(){
-	// hide elements bearing the js_hide class. oh, and flyins, too.
-	var $body = $('body');
-	$('.js_hide', $body).hide();
+		// hook up hyperlink functionality to eligible list items containing anchors
+		$('li > a, h1 a', contextEl).each(function(){
+			var $anchor = $(this);
+			var $parentItem = $anchor.parent();
+			if ($parentItem.css('cursor') == 'pointer') {
+				$parentItem.attr('alt', $anchor.attr('href'));
+				$anchor.removeAttr('href');
+				$parentItem.click(function(){
+					var href = $parentItem.attr('alt');
+					if (href != location.href) {
+						location.href = href;
+					}
+				});
+			}
+		});
+	};
 
-	// bind the hashchange event (for no-reload hyperlinks)
-	rewireHyperlinks($body);
-	$(window).hashchange( function(){
-		// we ignore hashes that don't start with this prefix
-		if ('#!/' == location.hash.substring(0, 3) && !lukep.pageRequestInProgress) {
-			lukep.pageRequestInProgress = true;
+	$(document).ready(function(){
+		// hide elements bearing the js_hide class. oh, and flyins, too.
+		var $body = $('body');
+		$('.js_hide', $body).hide();
 
-			var page_to_get = location.hash.substring(3);
-			$.ajax({
-				url: PAGE_FILE_DIR + page_to_get + PAGE_FILE_EXT,
-				success: function(data) {
-					var $new_content = $(data)
-						.find('.content')
+		// bind the hashchange event (for no-reload hyperlinks)
+		rewireHyperlinks($body);
+		$(w).hashchange( function(){
+			// we ignore hashes that don't start with this prefix
+			if ('#!/' == location.hash.substring(0, 3) && !app.pageRequestInProgress) {
+				app.pageRequestInProgress = true;
+
+				var page_to_get = location.hash.substring(3);
+				$.ajax({
+					url: PAGE_FILE_DIR + page_to_get + PAGE_FILE_EXT,
+					success: function(data) {
+						var $new_content = $(data)
+							.find('.content')
+								.removeClass('active_content')
+								.insertAfter('.active_content')
+								.css({'left': 700, 'display': 'block', 'opacity': 0})
+								.animate({'left': 0, 'opacity': 1}, 'slow');
+						$('.active_content')
 							.removeClass('active_content')
-							.insertAfter('.active_content')
-							.css({'left': 700, 'display': 'block', 'opacity': 0})
-							.animate({'left': 0, 'opacity': 1}, 'slow');
-					$('.active_content')
-						.removeClass('active_content')
-						.animate({'left': -700, 'opacity': 0}, 'slow', function() {
-							$new_content.addClass('active_content');
-							$(this).remove();
-							rewireHyperlinks($new_content);
-						});
-					lukep.pageRequestInProgress = false;
-				},
-				error: function(jqXHR, textStatus, errorThrown) {
-					alert("Sorry, the requested page couldn't be shown: \"" + errorThrown + "\"");
-					lukep.pageRequestInProgress = false;
-				},
-				dataType: 'html'
-			});
-		}
+							.animate({'left': -700, 'opacity': 0}, 'slow', function() {
+								$new_content.addClass('active_content');
+								$(this).remove();
+								rewireHyperlinks($new_content);
+							});
+						app.pageRequestInProgress = false;
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+						alert("Sorry, the requested page couldn't be shown: \"" + errorThrown + "\"");
+						app.pageRequestInProgress = false;
+					},
+					dataType: 'html'
+				});
+			}
+		});
+
+		// make our initial hashchange event fire
+		$(w).hashchange();
 	});
 
-	// make our initial hashchange event fire
-	$(window).hashchange();
-});
+	$(w).load(function(){
+		// do some intro animation or initialisation
+		setTimeout(function(){
+			app.pageRequestInProgress = false;
+		}, 1);
+	});
 
-$(window).load(function(){
-	// do some intro animation or initialisation
-	setTimeout(function(){
-		lukep.pageRequestInProgress = false;
-	}, 1);
-});
+})(window);
